b173eb4b1f71ee10516f26c822861a67368c7a00
suejung kang <crystal@sktai.io>
2023-05-02 01:51:21 +0000

    add file

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,6 +1,6 @@
 from skimage.metrics import peak_signal_noise_ratio as psnr, structural_similarity as ssim
-
-def evaluate_models(G, D_G, D_L, dataloader, discriminator_loss, criterion_context, generator_loss):
+import torch
+def evaluate_models(G, D_G, D_L, dataloader, discriminator_loss, criterion_context, generator_loss, face_parsing_model, device):
     G = G.eval()
     D_G = D_G.eval()
     D_L = D_L.eval()
@@ -17,40 +17,46 @@ def evaluate_models(G, D_G, D_L, dataloader, discriminator_loss, criterion_conte
             images = images.to(device)
             masks = masks.to(device)
             masked_images = masked_images.to(device) 
+            
+            # generator loss
+            first_out, second_out  = G(images, masks)
+            second_out_wholeimg = images * (1 - masks) + second_out * masks 
+            
+            loss_l1_1 = criterion_rec(first_out_wholeimg, images)
+            loss_l1_2 = criterion_rec(second_out_wholeimg, images)
+            loss_ssim_1 = criterion_ssim(first_out_wholeimg, images)
+            loss_ssim_2 = criterion_ssim(second_out_wholeimg, images)
+            loss_rec_1 = 0.5 * loss_l1_1 + 0.5 * (1 - loss_ssim_1)
+            loss_rec_2 = 0.5 * loss_l1_2 + 0.5 * (1 - loss_ssim_2)
 
-            completed_images = G(masked_images)
+            lambda_G = 1.0
+            lambda_rec_1 = 100.0
+            lambda_rec_2 = 100.0
+            lambda_per = 10.0
 
-            real_labels = torch.ones(images.size(0), 1).to(device)
-            fake_labels = torch.zeros(images.size(0), 1).to(device)
+            loss_P = criterion_parsing(face_parsing_model(second_out_wholeimg), face_parsing_model(images))
+            loss_generator = lambda_G * loss_G + lambda_rec_1 * loss_rec_1 + lambda_rec_2 * loss_rec_2 + lambda_per * loss_P
+            
+            local_real_D = D_L(images)
+            local_fake_D = D_L(second_out_wholeimg.detach())
 
-            local_real_output = D_L(images)
-            local_fake_output = D_L(completed_images)
+            global_real_D = D_G(images)
+            global_fake_D = D_G(second_out_wholeimg.detach())
 
-            global_real_output = D_G(images)
-            global_fake_output = D_G(completed_images)
+            loss_local_fake_D = criterion_adv(local_fake_D, target_is_real=False)
+            loss_local_real_D = criterion_adv(local_real_D, target_is_real=True)
 
-            # Expand the real and fake labels to match the output size of the discriminators
-            real_labels_expanded = real_labels.view(real_labels.size(0), 1, 1, 1).expand_as(global_real_output).clone()
-            fake_labels_expanded = fake_labels.view(fake_labels.size(0), 1, 1, 1).expand_as(global_fake_output).clone()
+            loss_global_fake_D = criterion_adv(global_fake_D, target_is_real=False)
+            loss_global_real_D = criterion_adv(global_real_D, target_is_real=True)
 
-            real_labels_expanded = real_labels_expanded.to(device)
-            fake_labels_expanded = fake_labels_expanded.to(device)
+            #gp = gradient_penalty(discriminator_global, images, completed_images)
+            loss_d = (loss_local_real_D + loss_local_fake_D + loss_global_fake_D + loss_global_real_D) * 0.25 #+ lambda_gp * gp
 
-            loss_adv_global = discriminator_loss(global_real_output, real_labels_expanded) + discriminator_loss(global_fake_output, fake_labels_expanded)
-            loss_adv_local = discriminator_loss(local_real_output, real_labels_expanded) + discriminator_loss(local_fake_output, fake_labels_expanded)
-            loss_adv = loss_adv_global + loss_adv_local 
-            loss_g_global = generator_loss(global_fake_output)
-            loss_g_local = generator_loss(local_fake_output)
 
-            expanded_masks = masks.expand_as(images)
-            loss_context = criterion_context(completed_images * expanded_masks, images * expanded_masks)
-            loss_perceptual = criterion_context(face_parsing_model(completed_images), face_parsing_model(images))
-
-            loss_generator = loss_g_global + loss_g_local + loss_context + loss_perceptual
-
+            
             total_gen_loss += loss_generator.item()
-            total_disc_global_loss += loss_adv_global.item()
-            total_disc_local_loss += loss_adv_local.item()
+            total_disc_global_loss += (loss_global_real_D + loss_global_fake_D).item()
+            total_disc_local_loss += (loss_local_real_D + loss_local_fake_D).item()
             total_samples += images.size(0)
 
     mean_gen_loss = total_gen_loss / total_samples
@@ -59,7 +65,7 @@ def evaluate_models(G, D_G, D_L, dataloader, discriminator_loss, criterion_conte
 
     return mean_gen_loss, mean_disc_global_loss, mean_disc_local_loss
 
-def evaluate_model_external(model, dataloader):
+def evaluate_model_external(model, dataloader, device):
     model.eval()
     total_psnr = 0
     total_ssim = 0
@@ -67,8 +73,9 @@ def evaluate_model_external(model, dataloader):
 
     with torch.no_grad():
         for i, (images, masks, masked_images) in enumerate(dataloader):
-            masked_images = masked_images.to(device) 
-            completed_images = model(masked_images)
+            images = images.to(device)
+            masks = masks.to(device) 
+            _, completed_images = model(images, masks)
  
             for i in range(images.size(0)):
                 image = images[i].cpu().numpy().transpose(1, 2, 0)
